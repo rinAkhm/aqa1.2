041dd43ce97d1260fc690bb59ee045bb43f4cfed
Rinat Akhmetzyanov <rin.akhm@gmail.com>
2020-01-25 20:33:37 +0300

    update code service

--- src/main/java/service/CashbackHackService.java
+++ src/main/java/service/CashbackHackService.java
@@ -1,9 +1,26 @@
 package service;
 
+//public class CashbackHackService {
+//    private final int boundary = 1000;
+//
+//    public int remain(int amount) {
+//        return boundary - amount % boundary;
+//    }
+//}
 public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        }
+
+        boolean isNeedMore = amount % boundary != 0;
+        if (!isNeedMore) {
+            return 0;
+        }
+
+        int remain = boundary - amount % boundary;
+        return remain;
     }
 }
