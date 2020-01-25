65c7c8375ae646a8dea178d9505b3e8abca7d0a6
Rinat Akhmetzyanov <rin.akhm@gmail.com>
2020-01-25 15:35:46 +0300

    Initial commit

---
+++ src/test/java/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturnZeroIfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn900IfAmount100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 100;
+        int actual = service.remain(amount);
+
+        int expected = 900;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn1500IfAmount500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int actual = service.remain(amount);
+
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+}
